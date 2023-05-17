795fb21b39f46c20af6a4b70d3ae80cd6b053607
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-05-16 21:52:10 -0300

    feat: top 5 categories implementada

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,30 @@
+from tech_news.database import db
+
+# import pymongo
+
+
 # Requisito 10
+
+# No MongoDB, ao usar a etapa $group em uma pipeline,
+#  é necessário usar o campo _id para especificar o
+# campo que será usado como identificador exclusivo
+# para agrupamento. O campo _id é especial e obriga-
+# tório na etapa $group.
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$category",
+                "count": {"$sum": 1},
+            }
+        },
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    # print(list(db.news.aggregate(pipeline)))
+    return [x["_id"] for x in list(db.news.aggregate(pipeline))]
+
+
+# print(top_5_categories())
